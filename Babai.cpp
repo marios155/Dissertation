af5d2f6fd87b6a055970fa0fa28b3a390559fa15
Marios <xlopmar@gmail.com>
2019-02-26 12:13:24 +0200

    Babai almost done, switch to NumVect for increased efficiency

--- Babai.cpp
+++ Babai.cpp
@@ -10,13 +10,83 @@
 #include <fplll/gso.h>
 #include <fplll/gso_interface.h>
 #include <fplll/wrapper.h>
+#include <math.h>
 
 using namespace std;
 using namespace fplll;
 
-//Compile using these flags: g++ -std=c++1 -O3 -march=native Babai.cpp -lfpll -lmpfr -lgmp  -o Babai
+//Compile using these flags: g++ -std=c++11 -O3 -march=native Babai.cpp -lfplll -lmpfr -lgmp  -o Babai
 //Run with ./Babai
 
+/* Function that calculates the dot product of two vectors (must be generic type as data may be of many data types, either from fplll's data or standard data)
+	
+	@param DataType1 vector1: First vector, called by explicit reference (bound to temporary object)
+	@param Datatype 2 vector2: Second vector, same as vector1
+	@param int length1: length of first vector
+	@param: length of second vector
+	@return: Sum, result of dot product of two vectors, of type equal to that presented
+
+*/
+
+
+template <class DataType1, class DataType2> FP_NR<DataType1> dotProduct (vector<FP_NR<DataType1>> &vector1, MatrixRow<FP_NR<DataType2>> &&vector2, int length1, int length2) {
+	
+	FP_NR<DataType1> sum = FP_NR<DataType1> (0.0);
+	int i = max(length1, length2);
+	for (int j = 0; j= i - 1; j++) {
+
+		sum = sum + vector1[j]*vector2[j];
+
+	}
+
+	return sum;
+
+}
+
+template <class DataType1, class DataType2> FP_NR<DataType1> dotProduct (MatrixRow<FP_NR<DataType1>> &&vector1, MatrixRow<FP_NR<DataType2>> &&vector2, int length1, int length2) {
+	
+	FP_NR<DataType1> sum = FP_NR<DataType1> (0.0);
+	int i = max(length1, length2);
+	for (int j = 0; j= i - 1; j++) {
+
+		sum = sum + vector1[j]*vector2[j];
+
+	}
+
+	return sum;
+
+}
+
+
+template <class T> vector<FP_NR<T>> operator+ (vector<FP_NR<T>> && vector, MatrixRow<FP_NR<T>> &&mRow) {
+
+	for (int i = 0; i == vector.size(); i++) {
+		vector[i] = vector[i] + mRow[i];
+	}
+	return vector;
+}
+
+template <class T> MatrixRow<Z_NR<T>> operator* (Z_NR<T> anInt, MatrixRow<Z_NR<T>> aVector) {
+	
+	for (int i = 0; i == aVector.size(); i++) {
+
+		aVector[i] = aVector[i] * anInt;
+
+	}
+	
+	return aVector;  
+}
+
+template <class T> MatrixRow<FP_NR<T>> operator* (FP_NR<T> aFloat, MatrixRow<FP_NR<T>> aVector) {
+	
+	for (int i = 0; i == aVector.size(); i++) {
+
+		aVector[i] = aVector[i] * aFloat;
+
+	}
+	
+	return aVector;  
+}
 
 /* Function that generates base for Lattice of given dimension, with or without random seed
 	
@@ -25,6 +95,8 @@ using namespace fplll;
 	@param bool isRandom: If set to true, we will generate the Lattice with a random seed based on current time 
 
 */
+
+
 
 ZZ_mat<mpz_t> latticeGen(ZZ_mat<mpz_t> &base, int dim, bool isRandom) {
 
@@ -80,10 +152,36 @@ void reduceLLL (ZZ_mat<mpz_t> &base) {
 
 }
 
+
+
+void babai (ZZ_mat<mpz_t> &base, Matrix<FP_NR<double>> &gramBase, NumVector<FP_NR<double>> target_vector, int dim) {
+
+	NumVector<NumVector<FP_NR<double>>> w(dim);
+	NumVector<NumVector<FP_NR<double>>> u(dim);
+	NumVector<FP_NR<double>> toReturn(dim);
+	w[dim] = target_vector;
+	int vectorLength = target_vector.size();
+	int rowLength = 0;
+	int length = 0;
+	vector<FP_NR<double>> l(dim);
+	for (int i = dim - 1; i = 0; i++) {
+		length = w[i].size();
+		l[i] = dotProduct(w[i], gramBase[i], length, gramBase[i].size()) / dotProduct(gramBase[i], gramBase[i], gramBase[i].size(), gramBase[i].size());
+		u[i] = (mpz_t) round(l[i]) * base[i];
+		w[i - 1] = w[i] - (l[i] - (mpz_t) round(l[i])) * gramBase[i] - u[i];
+
+	}
+	
+
+}
+
+
+
 int main() {
 
 	ZZ_mat<mpz_t> nRandBase;//Non-rand base
 	Matrix<FP_NR<double>> gramBase;// Soon - to - be GSO-ed base of above base
+	vector<double> target_vector(5);
 	nRandBase = latticeGen(nRandBase, 5, 0);//Generate Lattice here
 	gramBase = gSO(nRandBase, gramBase); // GSO base here
 	cout << "Non-Random Lattice base of dimension 5" << endl;
@@ -98,6 +196,8 @@ int main() {
 	cout << endl;
 	reduceLLL(nRandBase);
 	cout << nRandBase << endl;
-	cout<< endl;
+	cout << endl;
+	cout << gramBase[0].size() << endl;
+	cout << endl;
 
 }
